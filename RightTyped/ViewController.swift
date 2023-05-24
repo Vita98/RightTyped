0bb344b9c0d62fb6d260848bcc7c557e474323ae
Vitandrea Sorino <vitandrea98@live.it>
2023-05-24 20:27:49 +0200

    Initial project files

---
+++ RightTyped/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightTyped
+//
+//  Created by Vitandrea Sorino on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
